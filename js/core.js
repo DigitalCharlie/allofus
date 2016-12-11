d6e41db0fc9e7858a6d3c7980f4c895eddcf3189
Digital Charlie <furman.jr@gmail.com>
2016-12-11 01:40:49 -0800

    lots!

--- js/core.js
+++ js/core.js
@@ -11,36 +11,28 @@ $(function() {
 
 /* THIS IS THE HEADER SCROLL STUFF */
 
-  $(window).scroll(function() {    
-    var scroll = $(window).scrollTop();
-    if (scroll >= 500){
-      $(".header").css('height', '50px');
-    } else {
-      $(".header").css('height', '0');    
-    }
-  });
+  /* ALWAYS VISIBLE NOW
+    $(window).scroll(function() {    
+      var scroll = $(window).scrollTop();
+      if (scroll >= 500){
+        $(".header").css('height', '50px');
+      } else {
+        $(".header").css('height', '0');    
+      }
+    });
+  */
 
 /* Script Expand button */
 
-	$("#sample-script-button").click(function() {
-        $('#sample-script-button').css('display', 'none');
+	$("#homepage-read-more-link").click(function() {
+        $('#homepage-read-more-link').css('display', 'none');
         setTimeout(function() {
-        $('#sample-script-button').css('opacity', 0);
+        $('#homepage-read-more-link').css('opacity', 0);
         }, 50);
-        $('#script').css('display', 'block');
+        $('#homepage-read-more-content').css('display', 'block');
         setTimeout(function() {
-        $('.script').css('opacity', 1);
+        $('.homepage-read-more-content').css('opacity', 1);
         }, 50);;
-	})
-	$("#collapse-script").click(function() {
-		$('#script').css('display', 'none');
-        setTimeout(function() {
-        $('#script').css('opacity', 0);
-        }, 50);
-        $('#sample-script-button').css('display', 'block');
-        setTimeout(function() {
-        $('#sample-script-button').css('opacity', 1);
-        }, 50);
 	})
 
 });
